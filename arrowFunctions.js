9c0bc9b3cafe7ea552b020ddbc02634372431926
Octowl <alsaff1987@gmail.com>
2024-10-31 11:25:23 +0300

    task file

---
+++ arrowFunctions.js
@@ -0,0 +1,51 @@
+/**
+ * Rewrite the following `greet` function as an arrow function
+ */
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+/**
+ * sum(a, b):
+ * - revieces two numbers
+ * - returns their sum
+ * - write it as an arrow function
+ */
+function sum(a, b) {
+  // Your code here
+}
+
+/**
+ * square(n):
+ * - receives a number n
+ * - returns the square of the number
+ * - write it as an arrow function
+ *
+ * e.g.
+ * square(4) -> 16
+ * sqaure(10) -> 100
+ */
+function square(n) {
+  // Your code here
+}
+
+/**
+ * 🌶️🌶️
+ * sqaures(numbers):
+ * - receives an array numbers
+ * - returns an array of the squares of the numbers
+ * - write it as an arrow function
+ *
+ * e.g.
+ * squares([1, 2, 3]) -> [1, 4, 9]
+ */
+function sqaures(numbers) {
+  // Your code here
+}
+
+module.exports = {
+  greet,
+  sum,
+  square,
+  sqaures,
+};
